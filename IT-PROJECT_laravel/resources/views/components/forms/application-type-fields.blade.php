8a6f381dcffc92e3f5aacd3700967eaea4992d0c
aquino <user@example.com>
2025-10-13 00:34:31 +0800

    Update application type fields component

--- IT-PROJECT_laravel/resources/views/components/forms/application-type-fields.blade.php
+++ IT-PROJECT_laravel/resources/views/components/forms/application-type-fields.blade.php
@@ -2,23 +2,25 @@
     'form' => [
         'modification_reason' => '',
         'years' => '',
+        'permit_type' => '',
+        'applicationType' => '',
     ],
-    'applicationType' => '',
-    'permitTypeValue' => [],
     'showPermit' => false,
 ])
 
 <div class="form-field">
     <label>
-        <input type="radio" name="application_type" value="new" {{ $applicationType == 'new' ? 'checked' : '' }}>
+        <input type="radio" name="application_type" value="new"
+            {{ old('application_type', $form['application_type'] ?? '') === 'new' ? 'checked' : '' }}>
         NEW
     </label>
     <label>
-        <input type="radio" name="application_type" value="renewal" {{ $applicationType == 'renewal' ? 'checked' : '' }}>
+        <input type="radio" name="application_type" value="renewal"
+            {{ old('application_type', $form['application_type'] ?? '') === 'renewal' ? 'checked' : '' }}>
         RENEWAL</label>
     <label>
         <input type="radio" name="application_type" value="modification"
-            {{ $applicationType == 'modification' ? 'checked' : '' }}>
+            {{ old('application_type', $form['application_type'] ?? '') === 'modification' ? 'checked' : '' }}>
         MODIFICATION due to</label>
     @error('application_type')
         <p class="text-red text-sm mt-1">{{ $message }}</p>
@@ -29,18 +31,20 @@
     @error('modification_reason')
         <p class="text-red text-sm mt-1">{{ $message }}</p>
     @enderror
+
     @if ($showPermit)
         <label class="form-label" style="margin-top:12px;">Permit Type</label>
-        <label><input type="checkbox" name="permit_type" value="construction_permit"
-                {{ in_array('construction_permit', $permitTypeValue) ? 'checked' : '' }}>
+        <label><input type="radio" name="permit_type" value="construction_permit"
+                {{ old('permit_type', $form['permit_type'] ?? '') === 'construction_permit' ? 'checked' : '' }}>
             CONSTRUCTION PERMIT</label>
-        <label><input type="checkbox" name="permit_type" value="radio_station_license"
-                {{ in_array('radio_station_license', $permitTypeValue) ? 'checked' : '' }}>
+        <label><input type="radio" name="permit_type" value="radio_station_license"
+                {{ old('permit_type', $form['permit_type'] ?? '') === 'radio_station_license' ? 'checked' : '' }}>
             RADIO STATION LICENSE</label>
         @error('permit_type')
             <p class="text-red text-sm mt-1">{{ $message }}</p>
         @enderror
     @endif
+
     <label class="form-label">No. of Years</label>
     <input class="form1-01-input" type="text" name="years" placeholder="e.g., 2"
         value="{{ old('years', $form['years'] ?? '') }}">
