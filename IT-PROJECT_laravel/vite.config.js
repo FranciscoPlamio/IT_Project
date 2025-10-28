28cd1a4d3fd9380d88cf2696f5ea4559b7b332bc
aquino <user@example.com>
2025-10-28 22:49:33 +0800

    Updated vite config including all custom css and js files

--- IT-PROJECT_laravel/vite.config.js
+++ IT-PROJECT_laravel/vite.config.js
@@ -1,20 +1,33 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/adminside/index.js",
+                "resources/css/adminside/index.css",
+                "resources/js/adminside/req-management.js",
+                "resources/css/adminside/req-management.css",
+                "resources/js/adminside/dashboard.js",
+                "resources/css/adminside/dashboard.css",
+                "resources/js/adminside/cert-request.js",
+                "resources/css/adminside/cert-request.css",
+                "resources/js/adminside/bill-pay.js",
+                "resources/css/adminside/bill-pay.css",
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
-        host: 'localhost', // Allow external connections (change depending on your IP)
+        host: "localhost", // Allow external connections (change depending on your IP)
         port: 5173,
         hmr: {
-            host: 'localhost', //(change depending on your IP)
+            host: "localhost", //(change depending on your IP)
         },
     },
 });
